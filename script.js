0a710f2f791dd17eee9f3ce7a54712e85582772c
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-24 18:21:23 +0100

    ho spostato l'event listener al contenitore principale (wrapperEl), poi quando un quadrato viene cliccato, l'evento viene gestito dalla funzione che controlla se l'elemento cliccato è un quadrato (squareEl), risolto quindi il problema dell'evento click non chiamato correttamente

--- script.js
+++ script.js
@@ -23,44 +23,45 @@ function startNewGame(wrapperEl, modeSelector) {
     wrapperEl.innerHTML = '';
     const mode = parseInt(modeSelector.value);
     let cellsNumb = getNumberOfSquares(mode);
-    // bombe
     const bombs = genBombs(cellsNumb);
-
-    // 'Math.sqrt'static method returns the square root of a number (source: mdn)
     let squaresRow = Math.sqrt(cellsNumb);
+
     // ciclo for per generare gli squares
     for (let i = 0; i < cellsNumb; i++) {
         const singularSquare = createNewSquare();
         const squareContent = i + 1; 
         singularSquare.innerHTML += `<span> ${squareContent} </span>`;
-        // grandezze square
         const squareSize = `calc(100% / ${squaresRow})`;
         singularSquare.style.width = squareSize;
         singularSquare.style.height = squareSize;
         //creazione if che include anche le bombe + if per bg colors on click
         if (bombs.includes(squareContent)) {
-            singularSquare.classList.add('bomba');
-            singularSquare.innerHTML = '<i class="fas fa-bomb"></i>';
+            singularSquare.classList.add('bomba', 'bg-redbomb');
         } else {
             if (squareContent % 2 === 0) {
                 singularSquare.classList.add('bg-black');
             } else {
                 singularSquare.classList.add('bg-purple')
             }
-
-            // onf-off on click(toggle) + spostato nell'else il toggle realtivo al cambio bg-color 
-            singularSquare.addEventListener('click', function(){
-                if (singularSquare.classList.contains('bomba')) {
-                    endGame();
-                } else {
-                    singularSquare.classList.toggle('clicked');
-                    console.log(squareContent);
-                }
-            });
         }
         wrapperEl.appendChild(singularSquare);
     }
+    // creo un unico eventlistener collegato al wrapper principale
+    wrapperEl.addEventListener('click', function(event) {
+        const clickedSquare = event.target.closest('.squareEl');
+
+        if (!clickedSquare) return;
+        const squareContent = parseInt(clickedSquare.textContent.trim());
+
+        if (bombs.includes(squareContent)) {
+            endGame();
+        } else {
+            clickedSquare.classList.toggle('clicked');
+            console.log(squareContent);
+        }
+    });
 }
+
 // funzione per creare il quadratino
 function createNewSquare(){
     const newSquareEl = document.createElement('article');
@@ -99,5 +100,6 @@ function genBombs(cellsNumb) {
 }
     // step .3 creo un alert per mostrare al player che ha perso se clicca una bomba
 function endGame() {
+    console.log('endGame called'); 
     alert('BOOM! you clicked the wrong square, GAME OVER!')
 }
