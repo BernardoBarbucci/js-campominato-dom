714c8b80fa1eded047fec683c5ccb24042ba0571
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-24 17:25:34 +0100

    rifatta la funzione per determinare la difficolta (copiata da quella vecchia che ho rimosso e l'ho chiamata in modo diverso, uso quindi sempre lo switch)(special thanks goes to rc2pc2)

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione:
+// nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati:
+// - abbiamo calpestato una bomba
+// - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti
+// (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, 
+// cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
 // CODICE DELL'ESERCIZIO PRECEDENTE
 const gridEl = document.querySelector('.main-grid');
 const startButtonEl = document.querySelector('button#start-button');
@@ -14,20 +26,7 @@ function startNewGame(wrapperEl, modeSelector) {
     const squaresRow = Math.sqrt(cellsNumb);
     // bombe
     const bombs = genBombs(cellsNumb);
-
-    // varie difficolta (PROBABILMENTE DA RIMUOVERE)
-    // switch (mode){
-    //     case 1:
-    //         cellsNumb = 81;
-    //         break;
-    //     case 2:
-    //         cellsNumb = 49;
-    //         break;
-    //     default:
-    //         cellsNumb = 100;
-    //         break;
-    // }
-
+    
     // 'Math.sqrt'static method returns the square root of a number (source: mdn)
     let squaresRow = Math.sqrt(cellsNumb);
     // ciclo for per generare gli squares
@@ -58,9 +57,7 @@ function startNewGame(wrapperEl, modeSelector) {
                     console.log(squareContent);
                 }
             });
-
-
-
+        }
         wrapperEl.appendChild(singularSquare);
     }
 }
@@ -70,19 +67,20 @@ function createNewSquare(){
     newSquareEl.classList.add('squareEl');
     return newSquareEl;
 }
-// FINE CODICE DELL'ESERCIZIO PRECEDENTE
-
-    // CONSEGNA ESERCIZIO
-    // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-    // Attenzione:
-    // nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
-    // In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati:
-    // - abbiamo calpestato una bomba
-    // - la cella si colora di rosso e la partita termina.
-    // Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-    // La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti 
-    // (ovvero quando ha rivelato tutte le celle che non sono bombe).
-    // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-
 // generare 16 numeri casuali (prolly with an array) in ogni difficolta (quindi 16 numeri tra 1-) + attenzione che non possono essere generati gli stessi numeri = bombe 
 // aggiungo all'event listener del codice precedente un array per implementare le bombe e crearle automaticamente quando clicco il button per creare i quadratini
+
+    // step .1 rifaccio la funzione per le difficolta per semplificare l'aggiunta delle bombe
+function getNumberOfSquares(mode) {
+    switch (mode){
+        case 1:
+            cellsNumb = 81;
+            break;
+        case 2:
+            cellsNumb = 49;
+            break;
+        default:
+            cellsNumb = 100;
+            break;
+    }
+}
